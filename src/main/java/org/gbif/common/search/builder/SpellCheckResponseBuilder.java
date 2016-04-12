dda64013254e2c4a115adb9829230454bf4cebce
Federico Mendez <fmendez@gbif.org>
2016-04-12 11:52:08 +0200

    Utility methods created to handles cases when the result is built from collated results and suggestions

--- src/main/java/org/gbif/common/search/builder/SpellCheckResponseBuilder.java
+++ src/main/java/org/gbif/common/search/builder/SpellCheckResponseBuilder.java
@@ -6,8 +6,6 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
@@ -15,8 +13,6 @@ import com.google.common.collect.Maps;
  * Translates a Solr spell check response into a GBIF spell check response.
  */
 public class SpellCheckResponseBuilder {
-
-  private static final Joiner COLLATIONS_JOINER = Joiner.on(' ').skipNulls();
 
   /**
    * Utility classes hide constructors.
@@ -26,49 +22,64 @@ public class SpellCheckResponseBuilder {
   }
 
   public static SpellCheckResponse build(org.apache.solr.client.solrj.response.SpellCheckResponse solrSpellCheckResponse) {
-
     SpellCheckResponse spellCheckResponse = new SpellCheckResponse();
     spellCheckResponse.setCorrectlySpelled(solrSpellCheckResponse.isCorrectlySpelled());
-    Map<String,SpellCheckResponse.Suggestion> suggestionMap = Maps.newHashMap();
     //If the response contains collations, are use to build the response since those are guarantee to get results
     if (solrSpellCheckResponse.getCollatedResults() != null && !solrSpellCheckResponse.getCollatedResults().isEmpty()) {
-      for (org.apache.solr.client.solrj.response.SpellCheckResponse.Collation collation : solrSpellCheckResponse.getCollatedResults()) {
-        StringBuilder tokenBuilder = new StringBuilder();
-        StringBuilder spellCorrection = new StringBuilder();
-        for (Iterator<org.apache.solr.client.solrj.response.SpellCheckResponse.Correction>  itCorrections = collation.getMisspellingsAndCorrections().iterator();
-            itCorrections.hasNext();) {
-          org.apache.solr.client.solrj.response.SpellCheckResponse.Correction correction = itCorrections.next();
-          tokenBuilder.append(correction.getOriginal());
-          spellCorrection.append(correction.getCorrection());
-          if (itCorrections.hasNext()) {
-            tokenBuilder.append(' ');
-            spellCorrection.append(' ');
-          }
-        }
-        SpellCheckResponse.Suggestion suggestion = new SpellCheckResponse.Suggestion();
-        String token  = tokenBuilder.toString();
-        if (suggestionMap.containsKey(token)) {
-          List<String> alternatives = suggestionMap.get(token).getAlternatives();
-          alternatives.add(spellCorrection.toString());
-          suggestion.setAlternatives(alternatives);
-          suggestion.setNumFound(Math.max(suggestion.getNumFound(),new Long(collation.getNumberOfHits()).intValue()));
-        } else {
-          suggestion.setAlternatives(Lists.newArrayList(spellCorrection.toString()));
-          suggestionMap.put(token.toString(), suggestion);
-          suggestion.setNumFound(new Long(collation.getNumberOfHits()).intValue());
-        }
-
-      }
+      spellCheckResponse.setSuggestions(fromCollatedResults(solrSpellCheckResponse));
     } else if (solrSpellCheckResponse.getSuggestionMap() != null && !solrSpellCheckResponse.getSuggestionMap().isEmpty()) {
-      for (org.apache.solr.client.solrj.response.SpellCheckResponse.Suggestion solrSuggestion : solrSpellCheckResponse.getSuggestionMap()
-        .values()) {
+      spellCheckResponse.setSuggestions(fromSuggestionsResults(solrSpellCheckResponse));
+    }
+    return spellCheckResponse;
+  }
+
+  /**
+   * Creates a suggestions map from collated results.
+   */
+  private static Map<String,SpellCheckResponse.Suggestion> fromCollatedResults(org.apache.solr.client.solrj.response.SpellCheckResponse solrSpellCheckResponse) {
+    Map<String, SpellCheckResponse.Suggestion> suggestionMap = Maps.newHashMap();
+    for (org.apache.solr.client.solrj.response.SpellCheckResponse.Collation collation : solrSpellCheckResponse.getCollatedResults()) {
+      StringBuilder tokenBuilder = new StringBuilder(collation.getMisspellingsAndCorrections().size());
+      StringBuilder spellCorrection = new StringBuilder(collation.getMisspellingsAndCorrections().size());
+      for (Iterator<org.apache.solr.client.solrj.response.SpellCheckResponse.Correction> itCorrections =
+        collation.getMisspellingsAndCorrections().iterator(); itCorrections.hasNext();) {
+        org.apache.solr.client.solrj.response.SpellCheckResponse.Correction correction = itCorrections.next();
+        tokenBuilder.append(correction.getOriginal());
+        spellCorrection.append(correction.getCorrection());
+        if (itCorrections.hasNext()) {
+          tokenBuilder.append(' ');
+          spellCorrection.append(' ');
+        }
+      }
+      String token = tokenBuilder.toString();
+      if (suggestionMap.containsKey(token)) {
+        SpellCheckResponse.Suggestion suggestion = suggestionMap.get(token);
+        List<String> alternatives = suggestion.getAlternatives();
+        alternatives.add(spellCorrection.toString());
+        suggestion.setAlternatives(alternatives);
+        suggestion.setNumFound(Math.max(suggestion.getNumFound(), Long.valueOf(collation.getNumberOfHits()).intValue()));
+      } else {
         SpellCheckResponse.Suggestion suggestion = new SpellCheckResponse.Suggestion();
-        suggestion.setNumFound(solrSuggestion.getNumFound());
-        suggestion.setAlternatives(solrSuggestion.getAlternatives());
-        suggestionMap.put(solrSuggestion.getToken(), suggestion);
+        suggestion.setAlternatives(Lists.newArrayList(spellCorrection.toString()));
+        suggestion.setNumFound(Long.valueOf(collation.getNumberOfHits()).intValue());
+        suggestionMap.put(token.toString(), suggestion);
       }
     }
-    spellCheckResponse.setSuggestions(suggestionMap);
-    return spellCheckResponse;
+    return suggestionMap;
+  }
+
+  /**
+   * Creates a suggestions map from the Solr SpellCheckResponse.suggestionMap.
+   */
+  private static Map<String,SpellCheckResponse.Suggestion> fromSuggestionsResults(org.apache.solr.client.solrj.response.SpellCheckResponse solrSpellCheckResponse) {
+    Map<String,SpellCheckResponse.Suggestion> suggestionMap = Maps.newHashMap();
+    for (org.apache.solr.client.solrj.response.SpellCheckResponse.Suggestion solrSuggestion : solrSpellCheckResponse.getSuggestionMap()
+      .values()) {
+      SpellCheckResponse.Suggestion suggestion = new SpellCheckResponse.Suggestion();
+      suggestion.setNumFound(solrSuggestion.getNumFound());
+      suggestion.setAlternatives(solrSuggestion.getAlternatives());
+      suggestionMap.put(solrSuggestion.getToken(), suggestion);
+    }
+    return suggestionMap;
   }
 }
